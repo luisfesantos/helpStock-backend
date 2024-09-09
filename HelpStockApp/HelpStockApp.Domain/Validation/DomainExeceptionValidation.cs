9ce0badca5cda02c7a2e562498059c72e87342a4
luisfesantos <luis.santos585@etec.sp.gov.br>
2024-09-09 12:28:08 -0300

    Aula0909 - Validação de Domain Estrutural

---
+++ HelpStockApp/HelpStockApp.Domain/Validation/DomainExeceptionValidation.cs
@@ -0,0 +1,17 @@
+﻿namespace HelpStockApp.Domain.Validation
+{
+    public class DomainExeceptionValidation : Exception
+    {
+        public DomainExeceptionValidation(string error) : base(error)
+        {
+        }
+
+        public static void When(bool hasError, string error) 
+        {
+            if (hasError)
+            {
+                throw new DomainExeceptionValidation(error);
+            }
+        }
+    }
+}
